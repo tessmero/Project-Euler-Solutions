c3617e59d16c085febe5c8ba4557a08f244d22e6
Oliver Tessmer <oliver.tessmer@gmail.com>
2017-01-31 13:52:31 -0500

    added solver for problem 5

--- src/main/java/com/tessmero/projecteuler/solvers/Solver.java
+++ src/main/java/com/tessmero/projecteuler/solvers/Solver.java
@@ -15,7 +15,7 @@ public abstract class Solver {
    * @param args ignored
    */
   public static void main(String[] args) {
-    Solver instance = new Solver4();
+    Solver instance = new Solver5();
     System.out.println( instance.doSolution() );
   }
   
---
+++ src/main/java/com/tessmero/projecteuler/solvers/Solver5.java
@@ -0,0 +1,41 @@
+package com.tessmero.projecteuler.solvers;
+
+/**
+ * What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20.
+ * 
+ * @author Oliver
+ */
+public class Solver5 extends Solver{
+
+  @Override
+  public long doSolution() {
+    return getSmallestProduct( 20 );
+  }
+
+  @Override
+  public long doTest() {
+    return getSmallestProduct( 10 );
+  }
+
+  @Override
+  public long getExpectedTestResult() {
+    return 2520;
+  }
+  
+  private long getSmallestProduct( long largestFactor ) {
+    for ( long test = largestFactor ;; test += largestFactor ) {
+      if (isEvenlyDivisible(largestFactor, test)) {
+        return test;
+      }
+    }
+  }
+  
+  private boolean isEvenlyDivisible( long largestFactor, long test ) {
+    for ( int factor = 2 ; factor <= largestFactor ; factor++ ) {
+      if ( test % factor != 0 ) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
